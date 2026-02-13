eb5a12bb0d07be798ee6a0b83759c3dd8a948137
DJOU TOUSSE ERIC <eric.djou17@gmail.com>
2026-02-13 14:21:50 +0100

    fetch real resumes on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,35 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
 import type { Resume } from "../../types";
-
+import { usePuterStore } from "~/lib/puter";
+import { useEffect} from "react";
 
 const ResumeCard = ({
-    resume: { id, companyName, jobTitle, feedback, imagePath },
+  resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
-    resume: Resume;
-    }) => {
-    return (
+  resume: Resume;
+}) => {
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [imagePath]);
+  return (
     <Link
       to={`/resume/${id}`}
       className="resume-card animate-in fade-in duration-1000"
     >
-        <div className="resume-card-header">
+      <div className="resume-card-header">
         <div className="flex flex-col gap-2">
-        <h2 className="!text-black font-bold break-words">{companyName}</h2>
-        <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
-      </div>
-      <div className = "flex-shrink-0">
-        <ScoreCircle score={feedback.overallScore} />
-      </div>
+          {companyName && (
+            <h2 className="!text-black font-bold break-words">{companyName}</h2>
+          )}
+          {jobTitle && (
+            <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h3 className="!text-lg font-bold ">CV</h3>
+          )}
         </div>
+        <div className="flex-shrink-0">
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+      {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
-        <div className="w-full h-full">
-            <img src={imagePath} alt="resume"
-            className = "w-full h-[350px] max-sm:h-[200px] object-cover object-top" />
+          <div className="w-full h-full">
+            <img
+              src={resumeUrl}
+              alt="resume"
+              className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            />
+          </div>
         </div>
-        </div>
-     
+      )}
     </Link>
   );
 };
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,11 +1,12 @@
 import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
-import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
-
 import { usePuterStore } from "~/lib/puter";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import resume from "./resume";
+import { Link } from "react-router";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "CVmind" },
@@ -14,30 +15,77 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { auth } = usePuterStore();
-
+  const { auth, kv } = usePuterStore();
   const navigate = useNavigate();
+  const [resumeUrl, setResumeUrl] = useState("");
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
   useEffect(() => {
     if (!auth.isAuthenticated) {
-      navigate('/auth?next=/');
+      navigate("/auth?next=/");
     }
   }, [auth.isAuthenticated]);
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+      const resumes = (await kv.list("resume:*", true)) as KVItem[];
+      const parsedResumes = resumes.map(
+        (resume) => JSON.parse(resume.value) as Resume,
+      );
+      console.log(parsedResumes);
+      setResumes(parsedResumes || []);
+      setLoadingResumes(false);
+    };
+    loadResumes();
+  }, []);
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(resume.imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [resume.imagePath]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Mesurez la performance de votre CV et améliorez vos chances.</h1>
-          <h2>
-            Passez en revue vos candidatures et recevez des conseils
-            personnalisés par l’IA.
-          </h2>
+
+          {loadingResumes && resumes.length === 0 ? (
+            <h2>
+              Aucun CV disponible. Importez votre premier CV pour recevoir un
+              retour.
+            </h2>
+          ) : (
+            <h2>
+              Passez en revue vos candidatures et recevez des conseils
+              personnalisés par l’IA.
+            </h2>
+          )}
         </div>
-        {resumes.length > 0 && (
+        {loadingResumes && (
+          <div className="flex flex-col justify-center ">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+        )}
+        {!loadingResumes && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
             ))}
+          </div>
+        )}
+
+        {!loadingResumes && resume?.length === 0 && (
+          <div className="flex  flex-center justify-center items-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+            Télécharger un CV</Link>
           </div>
         )}
       </section>
